6caf97e3719d6eab461d8472a21e325222837272
Ngô Hải Văn <95236827+hvan128@users.noreply.github.com>
2025-05-17 11:09:04 +0700

    fix link to chrome

--- api/config/config.js
+++ api/config/config.js
@@ -2,6 +2,6 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   IS_RENDER_ENVIRONMENT: process.env.RENDER === 'true',
-  PUPPETEER_EXECUTABLE_PATH: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome',
+  PUPPETEER_EXECUTABLE_PATH: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
   USER_AGENT: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 };
